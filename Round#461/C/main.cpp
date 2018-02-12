fd91ac219a7b6b75dbd973ddd328cdd24c0f891a
Yuki-14544869 <a18916137180@gmail.com>
2018-02-12 23:58:05 +0800

    add C solution

--- Round#461/C/main.cpp
+++ Round#461/C/main.cpp
@@ -7,5 +7,12 @@ int main() {
     ios::sync_with_stdio(false);
     LL n, k;
     cin >> n >> k;
-    
+    for(int i=1; i<=k; ++i) {
+        if(n%i != i-1) {
+            cout <<"No" << endl;
+            return 0;
+        }
+    }
+    cout << "Yes\n";
+    return 0;
 }
